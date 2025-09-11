adfd6b0e79d8fb16d7fd63d5c0f55fea0c697c5a
Жолобов Дмитрий <dimazhol8967@gmail.com>
2025-09-11 11:46:23 +0300

    new

--- demo.py
+++ demo.py
@@ -1,5 +1,11 @@
 while True:
-    cmd = input("vfs>")
+    raw = input("vfs> ")
+    cmd, *args= raw.split()
     print(cmd)
     if cmd == "exit":
         break
+
+    if cmd == "ls":
+        print(cmd, args)
+    if cmd == "cd":
+        print(cmd, args)
