e64b17e4d6bd6be989e43a2d99cacb3b0c15c564
Жолобов Дмитрий <dimazhol8967@gmail.com>
2025-09-25 11:54:05 +0300

    Разработал эмулятор для языка оболочки ОС. Добавил заголовок VFS. Реализовал команду exit, парсер, команды заглушки.

--- demo.py
+++ demo.py
@@ -1,9 +1,12 @@
-# Вариант 40
 import  os
 def expand(s:str):
     for k in os.environ:
         s = s.replace(f'${k}', os.environ[k])
-        return s
+    return s
+
+def simple_parser(input_str):
+    parts = input_str.split()
+    return parts[0], parts[1:] if len(parts) > 1 else []
 
 while True:
     raw = expand(input("vfs> "))
@@ -11,7 +14,9 @@ while True:
     if cmd == "exit":
         break
 
-    if cmd == "ls":
+    elif cmd == "ls":
         print(cmd, args)
-    if cmd == "cd":
+    elif cmd == "cd":
         print(cmd, args)
+    else:
+        print(f"Неизвестная команда: {cmd}")
